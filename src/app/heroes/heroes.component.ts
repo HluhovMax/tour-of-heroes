27db30825261ac2c43808c1d06ab132224d9c48c
maksym.hlukhov <holosievo@gmail.com>
2020-03-04 10:32:32 +0200

    added in-memory DB

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
 import {HeroService} from '../hero.service';
-import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,14 +11,7 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
 
-  selectedHero: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) {
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService: Selected hero id=${hero.id}`);
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
